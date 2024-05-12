4be85ea1683caac9104f4fff5dfb139f11e465bc
SedgeDuck <wren.gray.pdx@gmail.com>
2024-05-12 12:08:38 -0700

    intro exercises done

---
+++ javascript.js
@@ -0,0 +1,76 @@
+let a = +prompt("a?");
+
+switch (a) {
+  case 0:
+    alert(0);
+    break;
+  case 1:
+    alert(1);
+    break;
+  case 2:
+  case 3:
+    alert("2,3");
+    break;
+}
+
+/*let browser = prompt("Please enter browser name");
+if (browser === "Edge") {
+  alert("You've got the Edge!");
+} else if (
+  browser === "Chrome" ||
+  browser === "Firefox" ||
+  browser === "Safari" ||
+  browser === "Opera"
+) {
+  alert("Okay we support these browsers too");
+} else {
+  alert("We hope this page looks ok!");
+}
+
+/*let message =
+  login === "Employee"
+    ? "Hello"
+    : login === "Director"
+    ? "Greetings"
+    : login === ""
+    ? "No login"
+    : "";
+
+/*let result = a + b < 4 ? "Below" : "Over";
+
+/*let num = prompt("Enter a number: ");
+
+if (num > 0) {
+  alert(1);
+} else if (num < 0) {
+  alert(-1);
+} else {
+  alert(0);
+}
+
+/*let javaName = prompt('What is the "official" name of JavaScript?');
+
+if (javaName === "ECMAScript") {
+  alert("Right!");
+} else {
+  alert("You don't know? ECMAScript!");
+}
+
+/*let userName = prompt();
+
+if (userName === "Admin") {
+  let password = prompt();
+
+  if (password === "BossBitch") {
+    alert("Welcome!");
+  } else if (password.length === 0) {
+    alert("Canceled");
+  } else {
+    alert("Incorrect password");
+  }
+} else if (userName.length === 0) {
+  alert("Canceled");
+} else {
+  alert("who dis");
+}
+*/
